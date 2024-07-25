424c647ee9d6b7b7fa786798754a16b752230d5c
Malik-Adil563 <madil4329@gmail.com>
2024-07-25 19:51:51 +0500

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -24,6 +24,15 @@ app.use(cors({
   credentials: true // If you need to include cookies in the requests
 }));
 
+ app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
+
 app.get('/', async (req, res) => {
   res.json("Hello");
 });
